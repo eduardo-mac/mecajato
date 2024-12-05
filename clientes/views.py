d6c947dc56ab14738bc90e0756e50703a7dc1e9c
eduardo <eduardosilvamacedo96@gmail.com>
2024-12-04 22:40:47 -0300

    commit 3

--- clientes/views.py
+++ clientes/views.py
@@ -2,4 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def clientes(request):
-    return render(request, 'clientes.html')
+    if request.method == "GET":
+        return render(request, 'clientes.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        ano = request.POST.getlist('ano')
+
+        print(nome)
+        print(carros)
+
+        return HttpResponse('teste')
+    
+
+    tempo: 101:20
+
+
